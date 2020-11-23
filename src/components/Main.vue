230cf1818e3991ce7c12be1f2d9bc80237387608
YuikoIto <toffywing@gmail.com>
2020-11-23 22:54:18 +0900

    delete: delete unnecessary files && change: set default layout

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid>
+    <v-layout justify-center>
+      <v-btn
+        color="primary"
+        outlined
+      >
+        ゲームスタート
+      </v-btn>
+    </v-layout>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+@Component
+export default class Main extends Vue{
+  
+}
+</script>
