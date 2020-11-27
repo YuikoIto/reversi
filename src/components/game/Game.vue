90c1220ac976345f5f7efeafab43dc65eaa4dc9e
YuikoIto <toffywing@gmail.com>
2020-11-27 22:11:20 +0900

    現在のターンがわかるように

--- src/components/game/Game.vue
+++ src/components/game/Game.vue
@@ -5,13 +5,16 @@
         :board="board"
       />
     </v-layout>
+    <v-layout justify-center>
+      <h2>{{ currentUser }}</h2>
+    </v-layout>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import BoardView from '@/components/game/BoardView.vue';
-import { Board } from '@/models/reversi';
+import { Board, CellState } from '@/models/reversi';
 @Component({
   components: {
     BoardView,
@@ -20,5 +23,14 @@ import { Board } from '@/models/reversi';
 
 export default class Game extends Vue {
   board: Board = new Board();
+  
+  get currentUser() {
+    if (this.board.turn === CellState.Black) {
+      return "黒のターンです"
+    } 
+    if (this.board.turn === CellState.White)  {
+      return "白のターンです"
+    }
+  }
 }
 </script>
